1e3d24a1f0088e816aac387db390ab1062df6d93
linxx5599 <linxx5599@163.com>
2024-11-07 00:54:53 +0800

    feat: add main

---
+++ main/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <h1 style="text-align: center;">主应用</h1>
+  <div id="app-container">
+    <router-view id='router-container' />
+  </div>
+</template>
+
+<script lang="ts" setup>
+</script>
+
+<style>
+#app-container {
+  font-family: Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  display: flex;
+  text-align: center;
+}
+
+#router-container {
+  flex: 1;
+}
+
+#public-links {
+  padding: 10px 0;
+}
+
+#public-links a {
+  text-decoration: underline;
+  color: -webkit-link;
+  cursor: pointer;
+}
+
+#public-links a:active {
+  color: #f53f3f;
+}
+</style>
